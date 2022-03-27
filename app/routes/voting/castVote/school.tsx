cccabe605c3bf66dcdd21d1d877dc05104f210df
Jack DeVries <jdevries3133@gmail.com>
2022-03-27 13:58:38 -0400

    fix: remove incorrect redirect call in frontend

--- app/routes/voting/castVote/school.tsx
+++ app/routes/voting/castVote/school.tsx
@@ -89,10 +89,6 @@ export default function School() {
   const [choice, setChoice] = useState("");
   const { posterA, posterB, error } = useLoaderData();
 
-  if (posterA === undefined) {
-    return redirect("/noMoreThings");
-  }
-
   if (error) {
     return (
       <>
